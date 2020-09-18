a5956f6e0b9d9661b71dbb9f858f131dffb81f78
meeresel <71521512+meeresel@users.noreply.github.com>
2020-09-18 21:18:39 +0300

    Add files via upload

---
+++ spring.lab1/spring.lab1/src/main/java/lr1/ist813/rozhkova/spring/lab1/Application.java
@@ -0,0 +1,13 @@
+package lr1.ist813.rozhkova.spring.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
